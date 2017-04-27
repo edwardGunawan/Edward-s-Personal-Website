a3d60e10839bf96a3fb8d2072feecf80e6d473b7
Edward Huang <egunawan@uscd.edu>
2017-04-27 01:50:28 -0400

    minifying everthing

--- js/script-production.js
+++ js/script-production.js
@@ -44,7 +44,7 @@ $(window).on('load',function(){
 
   var projectContent ={
     projects:[
-      { link:'#', name:'Portfolio', target:'', img:'http://edwardhuang1.me/image/compressed/Portfolio.min.jpg', class:'fade first-image-child'},
+      { link:'#', name:'Portfolio', target:'', img:'http://edwardhuang1.me/image/compressed/Portfolio.min.png', class:'fade first-image-child'},
       { link:'http://react-timer-app-gunawan.herokuapp.com/#/?_k=vfeyg7', target:'_blank', name:'ReactTimerApp', img:'http://edwardhuang1.me/image/compressed/ReactTimerApp-min.png', class:'fade last-two-image'},
       { link:'http://trackme-gunawan.herokuapp.com/#/?_k=i05q8p', target:'_blank', name:'TrackMe', img:'http://edwardhuang1.me/image/compressed/TrackMeResize-min.png', class:'fade last-two-image'}
     ]
